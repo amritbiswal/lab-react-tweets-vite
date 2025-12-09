dc19caa17b1b01c540067fc4b625572144b5a925
Amrit Biswal <amrit.k.biswal@gmail.com>
2025-12-09 15:07:57 +0100

    minor alignment changes

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,8 +1,10 @@
 // User component (not used in the final code)
 function User({ name, handle }) {
-  return (  <span className="user">
+  return (  
+  <span className="user">
     <span className="name">{name}</span>
     <span className="handle">@{handle}</span>
-  </span>);
+  </span>
+  );
 }
 export default User;
